605a08f6687c509a177614654879c72ff5638cb9
Saddam Hossain <saddam311291@gmail.com>
2025-05-20 13:31:44 +0600

    aa

---
+++ src/main/java/app/gradle/simple_app_gradle/HomeController.java
@@ -0,0 +1,14 @@
+package app.gradle.simple_app_gradle;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to the sidebar layout!");
+        return "home"; // this should match home.html under templates
+    }
+}
