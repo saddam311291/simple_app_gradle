be6581fe3adcee8893a4f03a8fdf2009d561d03f
Saddam Hossain <saddam311291@gmail.com>
2025-05-20 12:07:36 +0600

    init commit

---
+++ src/main/java/app/gradle/simple_app_gradle/SimpleAppGradleApplication.java
@@ -0,0 +1,13 @@
+package app.gradle.simple_app_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAppGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAppGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/gradle/simple_app_gradle/SimpleAppGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package app.gradle.simple_app_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAppGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
